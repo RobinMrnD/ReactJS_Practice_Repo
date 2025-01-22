70371fddd841581803bb015dd54437a3fd388631
RobinMrnD <miranda.robin.t@gmail.com>
2025-01-22 20:28:11 +0800

    userState() hook usage

--- SampleProject/src/App.jsx
+++ SampleProject/src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import MyComponent from "./components/MyComponent";
+import Counter from "./components/counter";
 
 function App() {
   return (
     <div>
-      
+      <Counter/>
     </div>
   );
 }
---
+++ SampleProject/src/components/MyComponent.jsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+
+function MyComponent(){
+
+    const [name, setName] = useState();
+    const [age, setAge] = useState(0);
+    const [isEmployed, setIsEmployed] = useState(false);
+
+    const updateName = () => {
+        setName("Robin")
+    }
+
+    const incrementAge = () =>{
+        setAge(age + 1);
+    }
+
+    const toggleEmpStat = () =>{
+        setIsEmployed(!isEmployed);
+    }
+
+    return(
+        <div>
+            <p>Name: {name}</p>
+            <button onClick={updateName}>Set Name</button>
+
+            <p>Age: {age}</p>
+            <button onClick={incrementAge}>Increment Age</button>
+
+            <p>Employed: {isEmployed ? "Yes": "No"}</p>
+            <button onClick={toggleEmpStat}>Change Employment Status</button>
+        </div>
+    )
+
+}
+
+export default MyComponent
