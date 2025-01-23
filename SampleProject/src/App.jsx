4e1c4bca013b911a9a87229ce656caf3a362df37
RobinMrnD <miranda.robin.t@gmail.com>
2025-01-23 11:42:28 +0800

    useEffect hooks

--- SampleProject/src/App.jsx
+++ SampleProject/src/App.jsx
@@ -1,9 +1,10 @@
 import MyComponent from "./components/MyComponent";
+import MyComponent2 from "./components/MyComponent2";
 
 function App() {
   return (
     <div>
-      <MyComponent/>
+      <MyComponent2/>
     </div>
   );
 }
--- SampleProject/src/components/MyComponent.jsx
+++ SampleProject/src/components/MyComponent.jsx
@@ -1,58 +1,32 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react'; // Added useEffect import
 
-function MyComponent(){ 
+function MyComponent() { 
+    const [count, setCount] = useState(0);
+    const [color, setColor] = useState("green")
 
-    const [games, setGames] = useState([]);
-    const [gameYear, setGameYear] = useState(new Date().getFullYear());
-    const [gameDeveloper, setGameDeveloper] = useState("");
-    const [gameName, setGameName] = useState("");
+    useEffect(() => {
+        document.title = `Count: ${count}  ${color}`;
+    }, [count, color]); 
 
-
-    function handleAddGame(){
-        const newGame = { year: gameYear,
-                          developer: gameDeveloper,
-                          name: gameName };
-        
-        setGames(g => [...g, newGame]);
-
-        setGameYear(new Date().getFullYear());
-        setGameDeveloper("");
-        setGameName("");
+    function addCount() {
+        setCount(c => c + 1);
+    }
+    function subtractCount() {
+        setCount(c => c - 1);
     }
 
-    function handleRemoveGames(index){
-        setGames (g => g.filter((_, i) => i !== index));
+    function changeColor() {
+        setColor(c => c === "green" ? "red" : "green")
     }
 
-    function handleYearChange(event){
-        setGameYear (event.target.value);
-    }
-
-    function handleDeveloperChange(event){
-        setGameDeveloper(event.target.value);
-    }
-    function handleNameChange(event){
-        setGameName(event.target.value);
-    }
-    return(
+    return (
         <div>
-          <h2>List of Games</h2>
-          <ul>
-            {games.map((game, index) => 
-                <li key={index} onClick={() => handleRemoveGames(index)}>
-                    {game.year} {game.developer} {game.name} 
-                </li>)}
-
-          </ul>
-            <input type="number" value={gameYear} onChange={handleYearChange}/> <br/>
-            <input type="text" value={gameDeveloper} onChange={handleDeveloperChange}
-                 placeholder ="Enter Game Developer"/><br/>
-            <input type="text" value={gameName} onChange={handleNameChange}
-                 placeholder ="Enter Game Name"/><br/>
-            <button onClick={handleAddGame}>Add Game</button>
+          <p style={{color:color}}>Count: {count}</p> 
+          <button onClick={addCount}>Add</button>
+          <button onClick={subtractCount}>Subtract</button>
+          <button onClick={changeColor}>Change Color</button>
         </div>
-    )
-
+    );
 }
 
-export default MyComponent
+export default MyComponent;
