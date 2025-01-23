e0ed184e525163becd252f898df522f73911d97b
RobinMrnD <miranda.robin.t@gmail.com>
2025-01-23 11:16:13 +0800

    To do list

--- SampleProject/src/App.jsx
+++ SampleProject/src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import ToDoList from "./component/ToDoList";
 
 function App() {
   return (
     <div>
-      
+      <ToDoList/>
     </div>
   );
 }
